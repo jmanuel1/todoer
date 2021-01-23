11326d9fa09365c144744d95d6c810dd0d3f434b
Jason Manuel <jama.indo@hotmail.com>
2021-01-22 22:29:56 -0700

    Make aesthetic changes to the about section
    Fixes #8.

--- lib/ui/about/about.jsx
+++ lib/ui/about/about.jsx
@@ -7,21 +7,24 @@ try {
 import { version } from '../../../package.json';
 
 export default function About() {
-  return (<dl>
-    <dt>Version</dt>
-    <dd>{version}</dd>
-    <dt>Authors</dt>
-    <dd>
-      <a href='http://jason-manuel.com'>Jason Manuel</a>
-      ... and <a href='https://github.com/jmanuel1/todoer'>contributors?</a>
-    </dd>
-    <dt>Copyright</dt>
-    <dd>2019-2021, Jason Manuel and Todoer Contributors</dd>
-    <dt>License</dt>
-    <dd>
-      <a href={`https://github.com/jmanuel1/todoer/blob/v${version}/LICENSE.md`}>
-        MIT
-      </a>
-    </dd>
-  </dl>);
+  return (<section>
+    <h6>About</h6>
+    <dl>
+      <dt>Version</dt>
+      <dd>{version}</dd>
+      <dt>Authors</dt>
+      <dd>
+        <a href='http://jason-manuel.com'>Jason Manuel</a>
+        ... and <a href='https://github.com/jmanuel1/todoer'>contributors</a>?
+      </dd>
+      <dt>Copyright</dt>
+      <dd>2019-2021, Jason Manuel and Todoer Contributors</dd>
+      <dt>License</dt>
+      <dd>
+        <a href={`https://github.com/jmanuel1/todoer/blob/v${version}/LICENSE.md`}>
+          MIT
+        </a>
+      </dd>
+    </dl>
+  </section>);
 }
