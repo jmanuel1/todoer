cd89ce376f4aaeb7a7a2f76060a7cd859b5c3e02
Jason Manuel <jama.indo@hotmail.com>
2021-06-19 18:36:58 -0700

    Make `Help` tests wait for the component to load the usage document

--- spec/ui/help/help-spec.jsx
+++ spec/ui/help/help-spec.jsx
@@ -1,36 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
+import { promises as fs } from 'fs';
 
 import jsDOMGlobal from 'jsdom-global';
 jsDOMGlobal();
 
 import Help from '../../../lib/ui/help/help';
 
-class Wrapper extends React.Component {
-  render() {
-    return <>{this.props.children}</>;
-  }
-}
-
 describe("The Help component", function () {
   beforeEach(function () {
-    this.component = ReactTestUtils.renderIntoDocument(
-      // See https://stackoverflow.com/a/36682433/3455228. Basically,
-      // renderIntoDocument doesn't support functional components, so it needs
-      // to be wrapped in a class component
-      <Wrapper><Help /></Wrapper>
-    );
-    const pre = ReactTestUtils.scryRenderedDOMComponentsWithTag(
-      this.component, 'pre')[0];
-    this.pre = pre;
+    // QUESTION: Can I make beforeEach async so that the test functions don't
+    // have to be?
+    this.component = new Promise(resolve => {
+      const component = ReactTestUtils.renderIntoDocument(
+        <Help onHelpLoad={() => {
+          const helpElement = ReactTestUtils.scryRenderedDOMComponentsWithTag(
+            component, 'section')[0];
+          this.helpElement = helpElement;
+          resolve(component);
+        }}/>
+      );
+    });
   });
 
-  it("should render into the page", function () {
-    expect(this.component).toBeDefined();
+  it("should render into the page", async function () {
+    expect(await this.component).toBeDefined();
   });
 
-  it("should contain the usage document", function () {
-    expect(this.pre.textContent).toContain("How to use todoer");
+  it("should contain the usage document", async function () {
+    await this.component;
+    expect(this.helpElement.textContent).toContain("How to use todoer");
   });
 });
